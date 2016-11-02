2a1a64753d2eddf3b6b746bb893dde590fe5baf9
liangyuzhe <liangyuzhe@yundianjia.com>
2016-11-02 20:22:12 +0800

    test

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in thetvdb.gemspec
+gemspec
+
+
+gem 'nokogiri'
+gem 'rest-client'
---
+++ lib/thetvdb.rb
@@ -0,0 +1,9 @@
+require "thetvdb/version"
+require 'rest-client'
+require 'nokogiri'
+
+
+require 'thetvdb/client'
+module Thetvdb
+  # Your code goes here...
+end
